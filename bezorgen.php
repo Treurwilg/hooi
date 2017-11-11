c4a54ecf1dbbe15c73d86ee8f323c0846e70621e
janKooijman <jcz.kooijman@hetnet.nl>
2017-11-11 19:24:30 +0100

    Commit vor stro-, vlas-, bezorgen-, en contactpagina. Contactformulier voor de verschillende pagina's nog te maken en email nog refereren.

---
+++ bezorgen.php
@@ -0,0 +1,19 @@
+<?php
+// bezorgen.php
+include("global.php");
+
+class ActueelPage extends Page {
+	
+	function printPageContent() {
+		echo file_get_contents("html/afstandsbalk80.html");
+		echo file_get_contents("html/afstandsbalk80.html");
+		echo file_get_contents("html/afstandsbalk40.html");
+		echo file_get_contents("html/bezorgenInfoBalk.html");
+		echo file_get_contents("html/afstandsbalk20.html");
+		echo file_get_contents("html/meerWetenBalk.html");
+		echo file_get_contents("html/onderTekstBalk.html");
+	}
+}
+$page = new ActueelPage();
+$page->render();
+?>
---
+++ stro.php
@@ -0,0 +1,20 @@
+<?php
+// stro.php
+include("global.php");
+
+class ActueelPage extends Page {
+	
+	function printPageContent() {
+		echo file_get_contents("html/afstandsbalk80.html");
+		echo file_get_contents("html/afstandsbalk80.html");
+		echo file_get_contents("html/afstandsbalk40.html");
+		echo file_get_contents("html/stroInfoBalk.html");
+		echo file_get_contents("html/stroFotoBalk.html");
+		echo file_get_contents("html/afstandsbalk20.html");
+		echo file_get_contents("html/meerWetenBalk.html");
+		echo file_get_contents("html/onderTekstBalk.html");
+	}
+}
+$page = new ActueelPage();
+$page->render();
+?>
